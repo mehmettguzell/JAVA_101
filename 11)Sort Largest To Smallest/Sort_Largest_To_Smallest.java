04aaf5f5deaca20160eeafd8521e11827bddfa7d
mehmettguzell <2107060@st.maltepe.edu.tr>
2024-05-09 21:34:47 +0300

    11

--- 11)Sort Largest To Smallest/Sort_Largest_To_Smallest.java
+++ 11)Sort Largest To Smallest/Sort_Largest_To_Smallest.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Sort_Largest_To_Smallest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
